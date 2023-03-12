23c4d17a1368c7c4c66857f1a10755a8a4cf9a8f
GSY <gle722@yandex.ru>
2023-03-12 12:55:17 +0300

    Написал метод создания массива

--- Реализация/Program.cs
+++ Реализация/Program.cs
@@ -3,4 +3,16 @@
    Клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется
    Пользоваться коллекциями, лучше обойтись исключительно массивами. */
 
-   
+string[] CreateArray()
+{
+    Console.Write("Введите размер массива: ");
+    int size = int.Parse(Console.ReadLine()!);
+    string[] array = new string[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write("Введите элемент массива: ");
+        array[i] = Console.ReadLine()!;
+    }
+    return array;
+}
+
